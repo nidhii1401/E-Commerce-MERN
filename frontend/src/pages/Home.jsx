8854ebe5694db6fb712bff499ba4ba898dd4a0b6
Nidhi Chaudhary <nivee.1401@gmail.com>
2025-12-01 19:47:33 +0530

    Added New Arrivals Page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from '../components/Layout/Hero'
 import GenderCollectionSection from '../components/Product/GenderCollectionSection'
+import NewArrivals from '../components/Product/NewArrivals'
 
 const Home = () => {
   return (
     <div>
         <Hero/> 
-        <GenderCollectionSection/>                 
+        <GenderCollectionSection/>   
+        <NewArrivals/>              
     </div>
   )
 }
