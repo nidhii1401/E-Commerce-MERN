554b69f7faaf5110a27cc0811eb04ce6f87862ce
Nidhi Chaudhary <nivee.1401@gmail.com>
2025-12-03 20:08:12 +0530

    Home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,6 +3,59 @@ import Hero from "../components/Layout/Hero";
 import GenderCollectionSection from "../components/Product/GenderCollectionSection";
 import NewArrivals from "../components/Product/NewArrivals";
 import ProductDetails from "../components/Product/ProductDetails";
+import ProductGrid from "../components/Product/ProductGrid";
+import FeaturedCollection from "../components/Product/FeaturedCollection";
+
+const placeholderProducts = [
+  {
+    _id: 1,
+    name: "Product 1",
+    price: 100,
+    images: [{ url: "https://picsum.photos/500?random=7" }],
+  },
+  {
+    _id: 2,
+    name: "Product 2",
+    price: 100,
+    images: [{ url: "https://picsum.photos/500?random=2" }],
+  },
+  {
+    _id: 3,
+    name: "Product 3",
+    price: 100,
+    images: [{ url: "https://picsum.photos/500?random=4" }],
+  },
+  {
+    _id: 4,
+    name: "Product 4",
+    price: 100,
+    images: [{ url: "https://picsum.photos/500?random=8" }],
+  },
+  {
+    _id: 5,
+    name: "Product 5",
+    price: 100,
+    images: [{ url: "https://picsum.photos/500?random=5" }],
+  },
+  {
+    _id: 6,
+    name: "Product 6",
+    price: 100,
+    images: [{ url: "https://picsum.photos/500?random=6" }],
+  },
+  {
+    _id: 7,
+    name: "Product 7",
+    price: 100,
+    images: [{ url: "https://picsum.photos/500?random=7" }],
+  },
+  {
+    _id: 8,
+    name: "Product 8",
+    price: 100,
+    images: [{ url: "https://picsum.photos/500?random=8" }],
+  },
+];
 
 const Home = () => {
   return (
@@ -16,6 +69,17 @@ const Home = () => {
         <h2 className="text-3xl font-bold mb-8 text-center">Best Sellers</h2>
         <ProductDetails />
       </section>
+
+      {/* Women's Collection */}
+      <div className="container mx-auto">
+        <h2 className="text-3xl text-center font-bold mb-4">
+          Top Wears for Women
+        </h2>
+        <ProductGrid product={placeholderProducts} />
+      </div>
+
+      {/* Featured Collection  */}
+      <FeaturedCollection />
     </div>
   );
 };
