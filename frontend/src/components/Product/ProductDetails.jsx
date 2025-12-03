107e3bc899e79af03425bbd8bf71eb595451e0ac
Nidhi Chaudhary <nivee.1401@gmail.com>
2025-12-03 19:50:02 +0530

    Product Details Page Completed

--- frontend/src/components/Product/ProductDetails.jsx
+++ frontend/src/components/Product/ProductDetails.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { toast } from "sonner";
+import ProductGrid from "./ProductGrid";
 
 const selectedProuduct = {
   name: "Stylish Jacket",
@@ -21,6 +22,34 @@ const selectedProuduct = {
     },
   ],
 };
+
+const similarProducts = [
+  {
+    _id: 1,
+    name: "Product 1",
+    price: 100,
+    images: [{ url: "https://picsum.photos/500?random=7" }],
+  },
+  {
+    _id: 2,
+    name: "Product 2",
+    price: 100,
+    images: [{ url: "https://picsum.photos/500?random=2" }],
+  },
+  {
+    _id: 3,
+    name: "Product 3",
+    price: 100,
+    images: [{ url: "https://picsum.photos/500?random=4" }],
+  },
+  {
+    _id: 4,
+    name: "Product 4",
+    price: 100,
+    images: [{ url: "https://picsum.photos/500?random=8" }],
+  },
+  
+];
 
 const ProductDetails = () => {
   const [mainImage, setMainImage] = useState(selectedProuduct.images[0]?.url);
@@ -291,6 +320,7 @@ const ProductDetails = () => {
           <h2 className="text-2xl text-center font-medium mb-4">
             You May Also Like
           </h2>
+          <ProductGrid product={similarProducts} />
         </div>
       </div>
     </div>
